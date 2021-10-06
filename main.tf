acd8ac3e927f3dd0bf0c586b2c821c0a5298b462
Sasano63 <sasano@hashicorp.com>
2021-10-06 10:42:35 +0200

    cd ad provider

--- main.tf
+++ main.tf
@@ -21,11 +21,7 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 provider "azuread" {
-  features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+    tenant_id       = var.tenant_id
 }
 resource "azurerm_resource_group" "vault-rg" {
   name     = "${var.prefix}-vault-rg"
