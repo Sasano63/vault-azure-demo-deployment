afb3d25c3e78ca4a971524747ef74e07864c4d43
Sasano63 <sasano@hashicorp.com>
2021-10-06 10:40:19 +0200

    + ad provider

--- main.tf
+++ main.tf
@@ -6,6 +6,10 @@ required_providers {
       source = "hashicorp/azurerm"
       version = ">=2.78.0"
     }
+   azuread = {
+      source = "hashicorp/azuread"
+      version = ">=2.5.0"
+    }
   }
 }
 
